44988579d3187a05192dbf250b321fadae4fb124
baipobng <baipobng@gmail.com>
2026-05-29 05:48:28 +1000

    update

--- backend/main.py
+++ backend/main.py
@@ -27,16 +27,24 @@ def home():
 @app.get("/chat")
 def chat():
 
-    response = client.chat.completions.create(
-        model="gpt-4.1-mini",
-        messages=[
-            {
-                "role": "user",
-                "content": "Hello"
-            }
-        ]
-    )
+    try:
 
-    return {
-        "reply": response.choices[0].message.content
-    }
+        response = client.chat.completions.create(
+            model="gpt-4.1-mini",
+            messages=[
+                {
+                    "role": "user",
+                    "content": "Hello"
+                }
+            ]
+        )
+
+        return {
+            "reply": response.choices[0].message.content
+        }
+
+    except Exception as e:
+
+        return {
+            "error": str(e)
+        }
